4721314a468e31025a1c3e5dab59bd296267461a
Jason Kay <geniture@me.com>
2012-08-08 14:49:10 -0400

    Remove railties req.

--- lib/activerecord-postgres-hstore.rb
+++ lib/activerecord-postgres-hstore.rb
@@ -1,4 +1,3 @@
-require "activerecord-postgres-hstore/railties"
 require "activerecord-postgres-hstore/string"
 require "activerecord-postgres-hstore/hash"
 require "activerecord-postgres-hstore/coder"
